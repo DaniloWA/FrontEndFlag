1f1926e3739a4b5cd863c83e177509bb78830bd4
Sérgio <srg.malves@gmail.com>
2022-03-21 20:54:02 +0000

    Fetch à api para listagem de produtos

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ import "./App.css";
 import fetchAPI from "./Middleware/getApi";
 import PageContextProvider from "./Services/pageContextProvider";
 import { Routes, Route } from "react-router-dom";
-import NotFound from "../src/Pages/NotFound/NotFound";
+import NotFound from "../src/Pages/notFound/NotFound";
 import Login from "./Pages/login/Login";
+import Products from "./Pages/products/Products";
 
 
 const App = () => {   
@@ -21,6 +22,7 @@ const App = () => {
 				<Routes>
 					<Route path="/login" element={<Login/>} />
 					<Route path="*" element={<NotFound />} />
+					<Route path="/products" element={<Products />} />
 				</Routes>
 			</PageContextProvider>
 		</div>
