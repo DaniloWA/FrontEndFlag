f6fbb44113733c8a4995b04a19078118ab73d320
Danilo <daniloworkdev@gmail.com>
2022-04-02 18:08:08 +0100

    Page login completa CSS

---
+++ src/Components/loading/Loading.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Loading.css";
+import iconsPath from "../../Assets/Images";
+const Loading = () => {
+	return ( 
+		<div className="Loading">
+			<div>
+				<img src={iconsPath.loading} alt="loading" />
+			</div>
+		</div>
+	);
+};
+ 
+export default Loading;
--- src/Layouts/navbar/Icon.jsx
+++ src/Layouts/navbar/Icon.jsx
@@ -7,57 +7,57 @@ import SideCar from "../../Components/SideCar/SideCar";
 import "../../Assets/Styles/NavBar.css";
 
 const Icon = () => {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const [showcart, setShowcart] = useState(false);
+	const [showcart, setShowcart] = useState(false);
 
-  return (
-    <div id="icons">
-      <ul>
-        <li className="usuario_select">
-          <img
-            src={iconsPath.user}
-            alt="Login"
-            onClick={() => navigate("/user")}
-          />
+	return (
+		<div id="icons">
+			<ul>
+				<li className="usuario_select">
+					<img
+						src={iconsPath.user}
+						alt="Login"
+						onClick={() => navigate("/user")}
+					/>
 
-          <span>Login</span>
-          <ul className="dropdowm_menu">
-            <li>
-              <a href="/login">Login</a>
-            </li>
-            <li>
-              <a href="/">Minha conta</a>
-            </li>
-            <li>
-              <a href="/login">Terminar sessão</a>
-            </li>
-          </ul>
-        </li>
-        <li className="favoritos">
-          <img
-            src={heart}
-            alt="Favoritos"
-            onClick={() => navigate("/Favoritos")}
-          />
-          <span>Favorites</span>
-        </li>
-        <li className="sacola">
-          <img
-            src={shoppingBag}
-            alt="Carrinho"
-            onClick={() => setShowcart(true)}
-          />
-          {showcart ? <SideCar /> : ""}
+					<span>Login</span>
+					<ul className="dropdowm_menu">
+						<li>
+							<a href="/login">Login</a>
+						</li>
+						<li>
+							<a href="/">Minha conta</a>
+						</li>
+						<li>
+							<a href="/login">Terminar sessão</a>
+						</li>
+					</ul>
+				</li>
+				<li className="favoritos">
+					<img
+						src={iconsPath.heart}
+						alt="Favoritos"
+						onClick={() => navigate("/Favoritos")}
+					/>
+					<span>Favorites</span>
+				</li>
+				<li className="sacola">
+					<img
+						src={iconsPath.shoppingBag}
+						alt="Carrinho"
+						onClick={() => setShowcart(true)}
+					/>
+					{showcart ? <SideCar /> : ""}
 
-          <span>Cart</span>
-          <span className="sacola_icon">
-            <span className="sacola_item">1</span>
-          </span>
-        </li>
-      </ul>
-    </div>
-  );
+					<span>Cart</span>
+					<span className="sacola_icon">
+						<span className="sacola_item">1</span>
+					</span>
+				</li>
+			</ul>
+		</div>
+	);
 };
 
 export default Icon;
