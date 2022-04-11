33e0e76026e371efaab3b95a0b0d88d5b2910219
Milena Fleming <milenasapucaia29@gmail.com>
2022-04-11 11:05:00 +0100

    Logic sum product's price

--- src/Components/SideCar/SideCar.jsx
+++ src/Components/SideCar/SideCar.jsx
@@ -11,6 +11,8 @@ const SideCar = ({setShowcart}) => {
 	const {car} = useData();
 	const [listCar, setListCar] = useState(car.products);
 	const [userCar] = useState(car.carUser);
+	const [subCar, setSubCar] = useState(0);
+
 
 	const style = {
 		estilo01: listCar && listCar.length != 0 ? "d-none" : "offcontent",
@@ -23,33 +25,43 @@ const SideCar = ({setShowcart}) => {
 		}else{
 			setListCar(car.products);
 		}
+		setSubCar(listCar?.reduce((acc, obj) => { 
+			let sum = obj.price;
+			if(obj.quantidade > 1) {
+				sum = obj.price * obj.quantidade;
+			}
+			return acc + sum;}, 0));
 	},[car]);
 	return (
 		<div className="SideCar">
 			<div className="title-button">
-				<h2>Carrinho</h2>
+				<h2>Cart</h2>
 				<button onClick={() => setShowcart(false)}>X</button>
 			</div>
 			<div className="content">
 				<ul className={style.estiloUL}> 
 					{listCar != null && userCar  != "anonymous" ? listCar.map((product) => {
 						return(
-							<ItemCar key={product.productId} title={product.title} quantidade={product.quantidade} id={product.productId} image={product.image} price={product.price}></ItemCar>
+							<ItemCar key={product.productId} title={product.title} quantidade={product.quantidade} id={product.productId} image={product.image} price={product.price}></ItemCar>	
 						);
 					}) : <Loading></Loading> }
 
 				</ul>
 				<div className={style.estilo01}>
 					<img src={iconsPath.iconCarrinho} alt="#" />
-					<h1>O teu carrinho está vazio.</h1>
+					<h1>Your cart is empty.</h1>
 					<Link to={"/"}>
 						<button onClick={() => setShowcart(false)} className="button">Começar a comprar</button>
 					</Link>
 				</div>
 			</div>
+	
 
 			<div className="valor">
-				<span>Subtotal</span> <span>0.00 €</span>
+			
+				
+				<span>Subtotal</span> <span>{subCar ? subCar.toFixed( 2 ) : 0} €</span>
+				
 			</div>
 			<div className="continue">
 				<Link to={routes.checkoutpage}>
